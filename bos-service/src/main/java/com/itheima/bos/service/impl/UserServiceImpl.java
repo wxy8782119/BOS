25e82a23338d2b418471494f405140a778ab3c68
wxy8782119 <635311890@qq.com>
2019-09-24 21:26:44 +0800

    实现用户登录功能和用户注销功能

---
+++ bos-service/src/main/java/com/itheima/bos/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.itheima.bos.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itheima.bos.dao.IUserDao;
+import com.itheima.bos.domain.User;
+import com.itheima.bos.service.IUserService;
+import com.itheima.bos.utils.MD5Utils;
+@Service
+@Transactional
+public class UserServiceImpl implements IUserService {
+	@Autowired
+	private IUserDao userDao;
+	/***
+	 * 用户登录
+	 */
+	public User login(User user) {
+		//使用MD5加密密码
+		String password = MD5Utils.md5(user.getPassword());
+		return userDao.findUserByUsernameAndPassword(user.getUsername(),password);
+		
+	}
+	
+}
