4f9784940eca3bb29a4b2e9b9de4663a29a8ef1e
wxy8782119 <635311890@qq.com>
2019-10-06 17:15:17 +0800

    定区关联客户（远程调用crm服务）

--- bos-web/src/main/java/com/itheima/bos/web/action/DecidedzoneAction.java
+++ bos-web/src/main/java/com/itheima/bos/web/action/DecidedzoneAction.java
@@ -1,6 +1,7 @@
 package com.itheima.bos.web.action;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import com.itheima.bos.domain.Decidedzone;
 import com.itheima.bos.service.IDecidedzoneService;
 import com.itheima.bos.web.action.base.BaseAction;
+import com.itheima.crm.Customer;
+import com.itheima.crm.ICustomerService;
 
 /**
  * 定区管理
@@ -42,5 +45,44 @@ public class DecidedzoneAction extends BaseAction<Decidedzone> {
 		decidedzoneService.pageQuery(pageBean);
 		this.java2json(pageBean, new String[] {"currentPage","detachedCriteria","pageSize","subareas","decidedzones"});
 		return NONE;
+	}
+	
+	//注入crm代理对象
+	@Autowired
+	private ICustomerService proxy;
+	
+	/**
+	 * 远程调用crm服务，获取未关联到定区的客户
+	 * @return
+	 */
+	public String findListNotAssociation() {
+		List<Customer> list = proxy.findListNotAssociation();
+		this.java2json(list, new String[] {});
+		return NONE;
+	}
+	
+	/**
+	 * 远程调用crm服务，获取已经关联到指定的定区的客户
+	 * @return
+	 */
+	public String findListHasAssociation() {
+		String id = model.getId();
+		List<Customer> list = proxy.findListHasAssociation(id);
+		this.java2json(list, new String[] {});
+		return NONE;
+	}
+	
+	//属性驱动，接收页面提交的多个客户id
+	private List<Integer> customerIds;
+	public void setCustomerIds(List<Integer> customerIds) {
+		this.customerIds = customerIds;
+	}
+
+	/**
+	 * 远程调用crm服务，将客户关联到定区
+	 */
+	public String assigncustomerstodecidedzone() {
+		proxy.assigncustomerstodecidedzone(model.getId(), customerIds);
+		return LIST;
 	}
 }
