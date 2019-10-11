96feeea29f60b0db715c94806241e164134442b5
wxy8782119 <635311890@qq.com>
2019-10-11 22:12:24 +0800

    在BOS项目中使用shiro进行认证

--- bos-dao/src/main/java/com/itheima/bos/dao/IUserDao.java
+++ bos-dao/src/main/java/com/itheima/bos/dao/IUserDao.java
@@ -7,4 +7,6 @@ public interface IUserDao extends IBaseDao<User> {
 
 	public User findUserByUsernameAndPassword(String username, String password);
 
+	public User findUserByUsername(String username);
+
 }
--- bos-web/src/main/java/com/itheima/bos/web/action/UserAction.java
+++ bos-web/src/main/java/com/itheima/bos/web/action/UserAction.java
@@ -3,6 +3,10 @@ package com.itheima.bos.web.action;
 import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.apache.struts2.ServletActionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import com.itheima.bos.domain.User;
 import com.itheima.bos.service.IUserService;
 import com.itheima.bos.utils.BOSUtils;
+import com.itheima.bos.utils.MD5Utils;
 import com.itheima.bos.web.action.base.BaseAction;
 
 @Controller
@@ -25,9 +30,36 @@ public class UserAction extends BaseAction<User> {
 	@Autowired
 	private IUserService userService;
 	/**
-	 * 用户登录
+	 * 用户登录，使用shiro框架提供的方式进行认证操作
 	 */
 	public String login() {
+		//从session中获取生成验证码
+		String validatecode = (String) ServletActionContext.getRequest().getSession().getAttribute("key");
+		//校验验证码是否输入正确
+		if(StringUtils.isNotBlank(checkcode) && checkcode.equals(validatecode)) {
+			//使用shiro框架提供的方式进行认证操作
+			Subject subject = SecurityUtils.getSubject();//获得当前用户对象，状态为“未认证”
+			AuthenticationToken token = new UsernamePasswordToken(model.getUsername(), MD5Utils.md5(model.getPassword()));//创建用户名密码令牌对象
+			try{
+				subject.login(token);
+			}catch (Exception e) {
+				e.printStackTrace();
+				return LOGIN;
+			}
+			User user = (User) subject.getPrincipal();
+			ServletActionContext.getRequest().getSession().setAttribute("loginUser", user);
+			return HOME;
+		}else {
+			//输入的验证码错误，设置提示信息，跳转到登录页面
+			this.addActionError("输入的验证码错误！");
+			return LOGIN;
+		}
+	}
+	
+	/**
+	 * 用户登录
+	 */
+	public String login_bak() {
 		//从session中获取生成验证码
 		String validatecode = (String) ServletActionContext.getRequest().getSession().getAttribute("key");
 		//校验验证码是否输入正确
